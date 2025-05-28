12cdd91c94ac19b0147b539544d3f62eeca57b21
Pradeep Panja <pradeep.panja@hut8.io>
2025-05-27 17:23:51 -0700

    Variables

---
+++ 01_variables_data_types.py
@@ -0,0 +1,13 @@
+my_fav_number = 13
+message = "I am learning AI with ChatGPT"
+slept_well = True
+
+
+print(type(my_fav_number))  # <class 'int'>
+print(type(message))  # <class 'str'>
+print(type(slept_well))  # <class 'bool'>
+
+# Print the values of the variables
+print("My favorite number is:", my_fav_number)
+print("Message:", message)
+print("Did I sleep well?", slept_well)
