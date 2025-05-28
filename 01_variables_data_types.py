36deee9d8f99bd4ce84ca6b400472154182d510f
Pradeep Panja <pradeepkpanja24@gmail.com>
2025-05-27 17:32:38 -0700

    "conversion added"

--- 01_variables_data_types.py
+++ 01_variables_data_types.py
@@ -1,11 +1,13 @@
 my_fav_number = 13
 message = "I am learning AI with ChatGPT"
 slept_well = True
+my_fav_number_str = str(my_fav_number)
 
 
 print(type(my_fav_number))  # <class 'int'>
 print(type(message))  # <class 'str'>
 print(type(slept_well))  # <class 'bool'>
+print(type(my_fav_number_str))  # <class 'str'>
 
 # Print the values of the variables
 print("My favorite number is:", my_fav_number, "and its type is", type(my_fav_number))
