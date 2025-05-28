764011f0ff9974173b02f89afb3de7062ee1c417
Pradeep Panja <pradeep.panja@hut8.io>
2025-05-27 17:03:05 -0700

    my first AI project

---
+++ test.py
@@ -0,0 +1,9 @@
+import datetime
+
+print("Hello, AI world")
+print("I am Pradeep Panja")
+print("Today is:", datetime.date.today())
+print("My learning goals:")
+print("-Master AI from scratch")
+print("-Bulit real world projects")
+print("-Become an expert under ChatGPT's guidance.")
