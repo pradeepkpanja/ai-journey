6710420dbe8fdd0d1f1afb709717a4fe56d4e9dc
Pradeep Panja <pradeepkpanja24@gmail.com>
2025-05-29 18:31:36 -0700

    Conditional statements updated

--- 03_conditional_statements.py
+++ 03_conditional_statements.py
@@ -1,28 +1,40 @@
-# If statement
-x = 10
-if x > 5:
-    print("x is greater than 5")
+#ask user input to positive or negative number
 
-# If-else statement
-y = 3
-if y % 2 == 0:
-    print("y is even")
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print(f"{number} is a positive number.")
+elif number < 0:
+    print(f"{number} is a negative number.")
+else:   
+    print("The number is zero.")
+
+
+# Check if the number is even or odd
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
 else:
-    print("y is odd")
+    print(f"{number} is an odd number.")
 
-# Elif statement
-z = 0
-if z > 0:
-    print("z is positive")
-elif z < 0:
-    print("z is negative")
+# Ask the user to enter percentage score
+
+percentage = float(input("Enter your percentage score: "))
+
+if percentage >= 85:
+    print("Distinction")
+elif percentage >= 50:
+    print("Pass")
 else:
-    print("z is zero")
+    print("Fail")
 
-# Nested if statement
-a = 15
-if a > 10:
-    if a % 2 == 0:
-        print("a is greater than 10 and even")
-    else:
-        print("a is greater than 10 but odd")
+#Ask the user to check age eligibility for AI roles
+age = int(input("Enter you age for AI roles: "))
+if age < 18:
+    print("You are not eligible for AI roles.")
+elif age < 60:
+    print("You are eligible for AI roles.")
+else:
+    print("Retirement? Still want to work in AI? That's impressive!")
+
+
